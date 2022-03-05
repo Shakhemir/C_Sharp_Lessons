cb96caeb4fb2c00e50d6c379e4a03af6df2250d9
Shakhemir <shakhemir@gmail.com>
2022-03-05 15:50:19 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int cube = new;
+
+for (int i = 1; i <= N; i++)
+{
+    cube += i * i * i;
+    Console.Write(cube);
+    if (i < N)
+    {
+        Console.Write(", ");
+    }
+}
+Console.WriteLine();
